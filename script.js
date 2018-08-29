9295deae7f9abb9898e66890958833d27b04319d
Unknown <zcole053@west-mec.org>
2018-08-29 15:27:40 -0700

    Server java
    I added a php file to connect to the server for uploads. I then went and added comments to the java script functions for better understanding. Then I added a function that has a try catch to prevent crashing when it request an object. After we added an if statement that activates the function when httpRequest is not false. Then for safety when a user spams the buttons more than once it will kill the extra clicks. After the abort is done then we set up an open to target a resource.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 /*  Exercise 01_11_01
 
     Whole Spectrum Energy Solutions
-    Author: 
-    Date:   
+    Author: Zedekiah Cole
+    Date: 8/29/18  
 
     Filename: script.js
 */
@@ -12,39 +12,67 @@
 // global variables
 var selectedCity = "Tucson, AZ";
 var weatherReport;
+var httpRequest = false; //have an XHR object to use
+
+//function to instatansite XHR object
+function getRequestObject() {
+    try {
+        httpRequest = new XMLHttpRequest();
+    } catch (requestError) {
+        document.querySelector("p.error").innerHTML = "Forecast not supported by your browser.";
+        document.querySelector("p.error").style.display = "block";
+        return;
+    }
+    return httpRequest;
+}
+
+//get weather on load event or click to get selected city
 
 function getWeather(evt) {
-   var latitude;
-   var longitude;
-   if (evt.type !== "load") {
-      if (evt.target) {
-         selectedCity = evt.target.innerHTML;
-      } else if (evt.srcElement) {
-         selectedCity = evt.srcElement.innerHTML;
-      }
-   }
-   if (selectedCity === "Tucson, AZ") {
-      latitude = 37.7577;
-      longitude = -122.4376;
-   } else if (selectedCity === "Chicago, IL") {
-      latitude = 41.8337329;
-      longitude = -87.7321555;
-   } else if (selectedCity === "Montreal, QC") {
-      latitude = 45.5601062;
-      longitude = -73.7120832;
-   }
+    var latitude;
+    var longitude;
+    if (evt.type !== "load") {
+        if (evt.target) {
+            selectedCity = evt.target.innerHTML;
+        } else if (evt.srcElement) {
+            selectedCity = evt.srcElement.innerHTML;
+        }
+    }
+    if (selectedCity === "Tucson, AZ") {
+        latitude = 37.7577;
+        longitude = -122.4376;
+    } else if (selectedCity === "Chicago, IL") {
+        latitude = 41.8337329;
+        longitude = -87.7321555;
+    } else if (selectedCity === "Montreal, QC") {
+        latitude = 45.5601062;
+        longitude = -73.7120832;
+    }
+    //check for XHR object or instantiate one
+    if (!httpRequest) {
+        httpRequest = getRequestObject();
+    }
+    // clear any open request
+    httpRequest.abort();
+    // target request to a resource
+    httpRequest.open("get" , "solar.php?" + "lat=" + latitude + "&lng=" + longitude);
 }
 
+//retrive location cities from the page 
 var locations = document.querySelectorAll("section ul li");
+
+//add click event listners for li elements
 for (var i = 0; i < locations.length; i++) {
-   if (locations[i].addEventListener) {
-      locations[i].addEventListener("click", getWeather, false);
-   } else if (locations[i].attachEvent) {
-      locations[i].attachEvent("onclick", getWeather);
-   }
+    if (locations[i].addEventListener) {
+        locations[i].addEventListener("click", getWeather, false);
+    } else if (locations[i].attachEvent) {
+        locations[i].attachEvent("onclick", getWeather);
+    }
 }
+
+//calls in finction when the page load
 if (window.addEventListener) {
-   window.addEventListener("load", getWeather, false);
+    window.addEventListener("load", getWeather, false);
 } else if (window.attachEvent) {
-   window.attachEvent("onload", getWeather);
+    window.attachEvent("onload", getWeather);
 }
--- solar.php
+++ solar.php
@@ -1,5 +1,5 @@
 <?php
-$WeatherSource = "https://api.forecast.io/forecast/apikey/" . $_GET["lat"] . "," . $_GET["lng"];
+$WeatherSource = "https://api.forecast.io/forecast/fc2698436e6d1419f04b76276cd12cf6/" . $_GET["lat"] . "," . $_GET["lng"];
 header("Content-Type: application/json");
 header("Cache-Control: no-cache");
 readfile($WeatherSource);
