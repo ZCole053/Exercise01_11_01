9e39a225e065ba13c1e4673bcbb083ca615a4d46
Unknown <zcole053@west-mec.org>
2018-08-31 15:15:19 -0700

    Visual data
    We added a for loop with a lop of if else statements. the for loop loops through the days and the data and applies it to the data per data table. We also added some math equation to the code to round out the data sent for the weather percentage.

--- script.js
+++ script.js
@@ -11,7 +11,7 @@
 
 // global variables
 var selectedCity = "Tucson, AZ";
-var weatherReport; //holds response data
+var weatherReport = null; //holds response data
 var httpRequest = false; //have an XHR object to use
 
 //function to instatansite XHR object
@@ -28,68 +28,110 @@ function getRequestObject() {
 
 
 //function to process reponse data
-function fillWeather(){
+function fillWeather() {
     //check reponse state for 4 (done) and status 200
-    if(httpRequest.readyState === 4 && httpRequest.status ===200){
+    if (httpRequest.readyState === 4 && httpRequest.status === 200) {
         weatherReport = JSON.parse(httpRequest.responseText);
-    }
-    var days = ["Sunday" , "Monday" ,"Tuesday" ,"Wednesday" , "Thursday" , "Friday" , "Saturday"];
-    var dateValue = new Date(weatherReport.daily.data[0].time);
-    var dateOfWeek = dateValue.getDay();
-    var rows = document.querySelectorAll("section.week table tbody tr");
-    document.querySelector("section.week table caption").innerHTML = selectedCity;
-}
-
-//get weather on load event or click to get selected city
-
-function getWeather(evt) {
-    var latitude;
-    var longitude;
-    if (evt.type !== "load") {
-        if (evt.target) {
-            selectedCity = evt.target.innerHTML;
-        } else if (evt.srcElement) {
-            selectedCity = evt.srcElement.innerHTML;
+        var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        var dateValue = new Date(weatherReport.daily.data[0].time);
+        var dayOfWeek = dateValue.getDay();
+        var rows = document.querySelectorAll("section.week table tbody tr");
+        document.querySelector("section.week table caption").innerHTML = selectedCity;
+        //populate rows with data
+        for (var i = 0; i < rows.length; i++) {
+            var firstCell = rows[i].getElementsByTagName("td")[0];
+            var secondCell = rows[i].getElementsByTagName("td")[1];
+            var thirdCell = rows[i].getElementsByTagName("td")[2];
+            firstCell.innerHTML = days[dayOfWeek];
+            //change day of the week in loop
+            if (dayOfWeek + 1 === 7) {
+                dayOfWeek = 0;
+            } else {
+                dayOfWeek++;
+            }
+                //find out the sun cover percentage
+                var sun = Math.round((1 - weatherReport.daily.data[i].cloudCover) * 100, 0);
+                //change symbol color based on sun percentage
+                if (sun > 90) {
+                    secondCell.style.color = "rgb(255,171,0)";
+                } else if (sun > 80 && sun <= 90) {
+                    secondCell.style.color = "rgb(255,179,25)";
+                } else if (sun > 70 && sun <= 80) {
+                    secondCell.style.color = "rgb(255,1188,51)";
+                } else if (sun > 60 && sun <= 70) {
+                    secondCell.style.color = "rgb(255,196,77)";
+                } else if (sun > 50 && sun <= 60) {
+                    secondCell.style.color = "rgb(255,205,102)";
+                } else if (sun > 40 && sun <= 50) {
+                    secondCell.style.color = "rgb(255,213,128)";
+                } else if (sun > 30 && sun <= 40) {
+                    secondCell.style.color = "rgb(255,221,153)";
+                } else if (sun > 20 && sun <= 30) {
+                    secondCell.style.color = "rgb(255,230,179)";
+                } else if (sun > 10 && sun <= 20) {
+                    secondCell.style.color = "rgb(255,238,204)";
+                } else if (sun <= 10) {
+                    secondCell.style.color = "rgb(255,247,230)";
+                }
+                secondCell.style.fontSize = "2.5em";
+                thirdCell.innerHTML = sun + "%";
+            }
+                document.querySelector("section.week table caption").style.display = "block";
+                document.querySelector("section.week table").style.display = "inline-block";
+                document.querySelector("section.week p.credit").style.display = "block";
+            }
         }
-    }
-    if (selectedCity === "Tucson, AZ") {
-        latitude = 37.7577;
-        longitude = -122.4376;
-    } else if (selectedCity === "Chicago, IL") {
-        latitude = 41.8337329;
-        longitude = -87.7321555;
-    } else if (selectedCity === "Montreal, QC") {
-        latitude = 45.5601062;
-        longitude = -73.7120832;
-    }
-    //check for XHR object or instantiate one
-    if (!httpRequest) {
-        httpRequest = getRequestObject();
-    }
-    // clear any open request
-    httpRequest.abort();
-    // target request to a resource
-    httpRequest.open("get" , "solar.php?" + "lat=" + latitude + "&lng=" + longitude , true);
-    //send request to server
-    httpRequest.send(null);
-    httpRequest.onreadystatechange = fillWeather;
-}
 
-//retrive location cities from the page 
-var locations = document.querySelectorAll("section ul li");
+        //get weather on load event or click to get selected city
 
-//add click event listners for li elements
-for (var i = 0; i < locations.length; i++) {
-    if (locations[i].addEventListener) {
-        locations[i].addEventListener("click", getWeather, false);
-    } else if (locations[i].attachEvent) {
-        locations[i].attachEvent("onclick", getWeather);
-    }
-}
+        function getWeather(evt) {
+            var latitude;
+            var longitude;
+            if (evt.type !== "load") {
+                if (evt.target) {
+                    selectedCity = evt.target.innerHTML;
+                } else if (evt.srcElement) {
+                    selectedCity = evt.srcElement.innerHTML;
+                }
+            }
+            if (selectedCity === "Tucson, AZ") {
+                latitude = 37.7577;
+                longitude = -122.4376;
+            } else if (selectedCity === "Chicago, IL") {
+                latitude = 41.8337329;
+                longitude = -87.7321555;
+            } else if (selectedCity === "Montreal, QC") {
+                latitude = 45.5601062;
+                longitude = -73.7120832;
+            }
+            //check for XHR object or instantiate one
+            if (!httpRequest) {
+                httpRequest = getRequestObject();
+            }
+            // clear any open request
+            httpRequest.abort();
+            // target request to a resource
+            httpRequest.open("get", "solar.php?" + "lat=" + latitude + "&lng=" + longitude, true);
+            //send request to server
+            httpRequest.send(null);
+            httpRequest.onreadystatechange = fillWeather;
+        }
+    
+        //retrive location cities from the page 
+        var locations = document.querySelectorAll("section ul li");
 
-//calls in finction when the page load
-if (window.addEventListener) {
-    window.addEventListener("load", getWeather, false);
-} else if (window.attachEvent) {
-    window.attachEvent("onload", getWeather);
-}
+        //add click event listners for li elements
+        for (var i = 0; i < locations.length; i++) {
+            if (locations[i].addEventListener) {
+                locations[i].addEventListener("click", getWeather, false);
+            } else if (locations[i].attachEvent) {
+                locations[i].attachEvent("onclick", getWeather);
+            }
+        }
+
+        //calls in finction when the page load
+        if (window.addEventListener) {
+            window.addEventListener("load", getWeather, false);
+        } else if (window.attachEvent) {
+            window.attachEvent("onload", getWeather);
+        }
